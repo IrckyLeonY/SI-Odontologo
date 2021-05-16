2bc5e98379f43c599fc9f382d536871461b6a07b
IrckyLeonY <84246072+IrckyLeonY@users.noreply.github.com>
2021-05-16 17:40:41 -0500

    Clase Paciente

--- src/sistema/odontologico/Paciente.java
+++ src/sistema/odontologico/Paciente.java
@@ -10,22 +10,47 @@ package sistema.odontologico;
  * @author riky_
  */
 public class Paciente {
-    private String Nombre; //20 = 40 bytes
-    private String Apellido; // 
-    private String Correo_Electronico;
-    private String Celular; //4 bytes
+    private String Nombres;//25=50
+    private String Apellidos;////25=50
+    private String Correo;//25=50
+    private String Telefono;//20
     private String FechaNacimiento;
-    private String Genero;
-    private String id_Clinico;
+    private String Genero;//16
+    private String Cedula;//20----//206
+          
 
-    public Paciente(String Nombre, String Apellido, String Correo_Electronico, String Celular, String FechaNacimiento, String Genero, String id_Clinico) {
-        this.Nombre = Nombre;
-        this.Apellido = Apellido;
-        this.Correo_Electronico = Correo_Electronico;
-        this.Celular = Celular;
-        this.FechaNacimiento = FechaNacimiento;
+    public Paciente(String Nombre, String Apellido, String Correo_Electronico, String Celular, String Genero, String Cedula) {
+        this.Nombres = Nombre;
+        this.Apellidos = Apellido;
+        this.Correo = Correo_Electronico;
+        this.Telefono = Celular;
         this.Genero = Genero;
-        this.id_Clinico = id_Clinico;
+        this.Cedula=Cedula;
+    }
+      public String getNombres() {
+        return Nombres;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+
+    public String getGenero() {
+        return Genero;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public String getTelefono() {
+        return Telefono;
     }
     
+    public String getCedula(){
+        return Cedula;
+    }
+    public int getTamanoPaciente(){
+     return Nombres.length()*2+ Apellidos.length()*2+Genero.length()*2+Correo.length()*2+Telefono.length()*2+Cedula.length()*2;
+    }
 }
